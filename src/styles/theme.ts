f83ea6e2f72ce691a102ec7dfb036d96cefea833
woong <ckehfqkr302@naver.com>
2025-02-14 16:38:53 +0900

    feat: 기본 테마 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  white: '#FFFFFF',
+  primary: '#FA2454',
+  primaryActive: '#E02448',
+  primary100: '#F95F7B',
+  secondary: '#A31735',
+  secondaryHover: '#8B1F2A',
+  black: '#000000',
+  darkGray: '#111113',
+  darkGray80: '#1A1A1D',
+  darkGray50: '#2A2A2C',
+  lightGray: '#CCCCCC',
+};
+
+export const theme = {
+  colors,
+};
+
+export type ThemeType = typeof theme;
