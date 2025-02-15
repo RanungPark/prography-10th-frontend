18a49e4651ffb1d321db596ddb40f9b5fd4045f0
woong <ckehfqkr302@naver.com>
2025-02-15 20:55:04 +0900

    feat: 글로벌스타일, theme 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import Router from './router';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/GlobalStyles';
 
-createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  </StrictMode>
+);
